e36e1c71b3e4a73c3d5a594f381e3f5c695a9d26
Kiran Sanubala <83561712+Niravthebest@users.noreply.github.com>
2022-05-20 16:24:32 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+// Create function addUser()
+function addUser() {
+  // Get value of user by id player1_name_input and player2_name_input
+    var player1_name = document.getElementById("player1_name_input").value;
+    var player2_name = document.getElementById("player2_name_input").value;
+  // Store these values locally
+    localStorage.setItem(player1_name);
+    localStorage.setItem(player2_name);
+  //Assign "game_page.html" to window.location
+    window.location = "game_page.html";
+}
+
